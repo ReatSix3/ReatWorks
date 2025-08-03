cc5c19dbb65c4b72556538e9590381c0e36b1291
Reat <daffapasya1906@gmail.com>
2025-08-03 15:18:36 +0700

    bgm

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleSound() {
+  const btn = document.getElementById("soundButton");
+  const audio = document.getElementById("bgMusic");
+  audio.play()
+
+  if (btn.classList.contains("sound-on")) {
+    btn.classList.remove("sound-on");
+    btn.classList.add("sound-off");
+    audio.volume = 0;
+  } else {
+    btn.classList.remove("sound-off");
+    btn.classList.add("sound-on");
+    audio.volume = 0.5;
+  }
+}
+const button = document.getElementById("soundButton");
+button.classList.toggle("sound-off");
+
